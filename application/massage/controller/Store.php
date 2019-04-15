6ee0f19bd8041ad8ad16fbe0630b148178c43c02
serena <1229046791@qq.com>
2019-04-15 18:46:23 +0800

    '预约列表详情，添加滑动插件，添加地址图片'

--- application/massage/controller/Store.php
+++ application/massage/controller/Store.php
@@ -9,9 +9,12 @@
 
 namespace app\massage\controller;
 
+use think\App;
 use think\facade\Cache;
 use think\Controller;
 use think\Db;
+use think\facade\Config;
+use think\facade\Request;
 
 class Store extends Controller
 {
@@ -31,24 +34,44 @@ class Store extends Controller
         return json('200','推拿门店获取成功',count(Cache::get('store_list')),Cache::get('store_list'));
     }
 
-//    public function initialize()
-//    {
-//        if(!parent::initialize()){
-//            Header("Location: error.html");
-//            exit;
-//        }
-//
-//        if(empty(Cookie::get('u_mobile'))){
-//            Header("Location: register.html");
-//        }
-//    }
-//
-//    public function index(){
-//        return $this->fetch();
-//    }
-//
     //显示推拿门店列表
     public function massageList(){
+        $data = Request::instance()->get();
+        $list = Db::name('massage_store')->where($data)->find();
+        $i = 0;
+        $time = array("日","一","二","三","四","五","六");
+
+        $work = explode(',',$list['ms_workShift']);
+        $massage_time = Config::get('app.massage_time');
+        $workShift = array();
+        foreach ($work as $key=>$value){
+            $workShift[$key] = $massage_time[$value];
+        }
+
+        while ($i <= $list['ms_slot'])
+        {
+            if($i == 0){
+                $list['slot'][$i] = '今天';
+                $list['time'][$i] = date("Y年m月d日",strtotime("+".$i." day")). "";
+                $list['workShift'][$i] = $workShift;
+            }elseif ($i == 1){
+                $list['slot'][$i] = '明天';
+                $list['time'][$i] = date("Y年m月d日",strtotime("+".$i." day")). "";
+
+                $list['workShift'][$i] = $workShift;
+            }elseif ($i == 2){
+                $list['slot'][$i] = '后天';
+                $list['time'][$i] = date("Y年m月d日",strtotime("+".$i." day")). "";
+
+                $list['workShift'][$i] = $workShift;
+            }else{
+                $list['slot'][$i] = '星期'.$time[date("w",strtotime("+".$i." day"))];
+                $list['time'][$i] = date("Y年m月d日",strtotime("+".$i." day")). "";
+                $list['workShift'][$i] = $workShift;
+            }
+            $i ++;
+        }
+        $this->assign('data',$list);
         return $this->fetch();
     }
 
